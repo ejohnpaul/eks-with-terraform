ed696c9ebe799bfc399e944d5af26fb38999de31
ejohnpaul <johnpaulcemenogu@gmail.com>
2023-10-06 12:52:05 -0400

    creste script modified

--- create-script.sh
+++ create-script.sh
@@ -1,23 +1,37 @@
 #!/bin/bash
 # To create the VPC that will host the EKS cluster
-cd vpc
+
+cd vpc || exit
 terraform init
 terraform fmt
 terraform validate && terraform plan >vpcplan
+
+if [ "$?" -eq "0" ]; then
+  echo "Your code has been successfully validated"
+  echo "You can view the plan file in vpcplan"
+  sleep 2
+else
+  echo "The code needs some review!"
+  exit
+fi
 terraform apply -auto-approve
-#sleep 240
+
 # change directory to cluster after VPC is created
-cd ../cluster
+cd ../cluster || exit
 # Create the EKS cluster
 terraform init
 terraform fmt
 terraform validate && terraform plan >clusterplan
+
+if [ "$?" -eq "0" ]; then
+  echo "The code has been successfully validated"
+  echo "You can review the plan file in clusterplan"
+  sleep 2
+else
+  echo "The code needs some review!"
+  exit
+fi
 terraform apply -auto-approve
-#sleep 300
-# change diretory to users and create users, groups, and roles
-cd ../users
-terraform init
-terraform fmt
-terraform validate && terraform plan >usersplan
-terraform apply -auto-approve
-echo "VPC, Cluster, Users created
+
+echo "VPC and EKS Cluster are created successfully"
+exit
