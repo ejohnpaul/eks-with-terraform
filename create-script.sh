dc3b5c8f290d4783965df62e483d7174995481b1
ejohnpaul <johnpaulcemenogu@gmail.com>
2023-10-06 12:47:51 -0400

    create script

--- create-script.sh
+++ create-script.sh
@@ -5,7 +5,6 @@ terraform init
 terraform fmt
 terraform validate && terraform plan >vpcplan
 terraform apply -auto-approve
-#sleep 240
 # change directory to cluster after VPC is created
 cd ../cluster
 # Create the EKS cluster
@@ -13,7 +12,6 @@ terraform init
 terraform fmt
 terraform validate && terraform plan >clusterplan
 terraform apply -auto-approve
-#sleep 300
 # change diretory to users and create users, groups, and roles
 cd ../users
 terraform init
